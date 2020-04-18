d92c436cba1cfcfd048645e1500dcfb46268ab7e
m-ssilva <bleed-it-out@live.com>
2020-04-17 22:21:41 -0300

    feat: 🎸 add healthcheck of database
    added health check of database on /api/health endpoint

--- src/services/health.service.js
+++ src/services/health.service.js
@@ -1,6 +1,16 @@
+const { sequelize: db } = require('../models')
+
+const checkDbHealth = () =>
+  db.authenticate()
+    .then(() => true)
+    .catch(() => false)
+
 const get = async ctx => {
   ctx.status = 200
-  ctx.body = { health: true }
+  ctx.body = {
+    api: true,
+    database: await checkDbHealth()
+  }
 }
 
 module.exports = { get }
